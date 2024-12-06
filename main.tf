2509555aa099fa211caa3f66f0e9432a0b7cb102
AnilTeku <tekumallaa@outlook.com>
2024-12-06 16:31:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  subscription_id = "acec003b-caf1-482b-ba40-29a3cdd690f9"
+  client_id       = "b9079c46-ddcd-4c4f-bacd-ddc988eb6a5e"
+  client_secret   = "psJ8Q~1yviUpp4yvgThZM5HaLoZ57VlSSa6uWa0u"
+  tenant_id       = "afa32410-c43f-47dd-be3a-654fe6e46b88"
 }
 
 resource "azurerm_resource_group" "test_rg" {
