1d33a0fa1b0f4087643ff332feb790b5ef10652c
AnilTeku <tekumallaa@outlook.com>
2024-12-06 15:33:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "test_rg1" {
+resource "azurerm_resource_group" "test_rg" {
   name     = "TestResourceGroup"
   location = "East US"
 }
