1d82b32ce590e8ef436cdfc918a51a904836a24a
AnilTeku <tekumallaa@outlook.com>
2024-12-06 16:51:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,11 +16,66 @@ provider "azurerm" {
   tenant_id       = "afa32410-c43f-47dd-be3a-654fe6e46b88"
 }
 
-resource "azurerm_resource_group" "test_rg" {
-  name     = "TestResourceGroup"
+# Create a Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
   location = "East US"
 }
 
-output "resource_group_name" {
-  value = azurerm_resource_group.test_rg.name
+# Create a Virtual Network
+resource "azurerm_virtual_network" "example_vnet" {
+  name                = "example-vnet"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Create a Storage Account
+resource "azurerm_storage_account" "example_storage" {
+  name                     = "examplestorageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier              = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Create a Subnet for Virtual Machine
+resource "azurerm_subnet" "example_subnet" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create Network Interface
+resource "azurerm_network_interface" "example_nic" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "example-ip-config"
+    subnet_id                    = azurerm_subnet.example_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Create a Virtual Machine
+resource "azurerm_linux_virtual_machine" "example_vm" {
+  name                = "example-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  admin_password      = "P@ssw0rd1234"
+  network_interface_ids = [
+    azurerm_network_interface.example_nic.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  tags = {
+    environment = "testing"
+  }
 }
